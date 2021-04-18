21fa638ffffc29c21963146d07b55bdd52c3d4c7
Axel Ainsaar <axel.ainsaar@klarna.com>
2021-04-19 00:55:19 +0200

    Initial Commit

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -1,16 +1,63 @@
-import React from 'react';
-import { Paragraph } from '@contentful/forma-36-react-components';
+import React, { useEffect, useState } from 'react';
+import { Paragraph, Spinner } from '@contentful/forma-36-react-components';
 import { FieldExtensionSDK } from '@contentful/app-sdk';
 
 interface FieldProps {
   sdk: FieldExtensionSDK;
 }
 
-const Field = (props: FieldProps) => {
-  // If you only want to extend Contentful's default editing experience
-  // reuse Contentful's editor components
-  // -> https://www.contentful.com/developers/docs/extensibility/field-editors/
-  return <Paragraph>Hello Entry Field Component</Paragraph>;
+const Field = ({ sdk }: FieldProps) => {
+  const [ingredient, setIngredient] = useState('')
+  const [amount, setAmount] = useState(sdk.entry.fields.amount.getValue())
+  const [measurement, setMeasurement] = useState('')
+
+  useEffect(() => {
+    const detachIngredientChange = sdk.entry.fields.ingredient.onValueChanged(ingredient => {
+      if (ingredient) {
+        sdk.space.getEntry(ingredient.sys.id).then((ingredient: any) => {
+          setIngredient(ingredient.fields.name['sv-SE'])
+        })
+      }
+    })
+
+    const detachAmountChange = sdk.entry.fields.amount.onValueChanged(amount => {
+      setAmount(amount)
+    })
+
+    const detachMeasurementChange = sdk.entry.fields.measurement.onValueChanged(measurement => {
+      if (measurement) {
+        sdk.space.getEntry(measurement.sys.id).then((measurement: any) => {
+          setMeasurement(measurement.fields.name['sv-SE'])
+        })
+      } else {
+        setMeasurement('')
+      }
+    })
+    
+    return () => {
+      detachIngredientChange()
+      detachAmountChange()
+      detachMeasurementChange()
+    }
+  }, [sdk.space, sdk.entry.fields.ingredient, sdk.entry.fields.amount, sdk.entry.fields.measurement]);
+
+  if (!ingredient) {
+    return <Spinner />
+  }
+
+  let title = ingredient;
+
+  if (amount) {
+    title += ` ${amount}`
+  }
+
+  if (measurement) {
+    title += ` ${measurement}`
+  }
+
+  sdk.field.setValue(title)
+
+  return <Paragraph>{title}</Paragraph>
 };
 
 export default Field;
